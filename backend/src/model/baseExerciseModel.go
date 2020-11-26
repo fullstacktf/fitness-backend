ddcb2151459a83ba3ad3357cc8ab43963d6eec10
Raul Rodriguez Torres <raulrodrigueztorres@protonmail.com>
2020-11-26 20:00:30 +0000

    Use of gorm.Model applied

--- backend/src/model/baseExerciseModel.go
+++ backend/src/model/baseExerciseModel.go
@@ -1,15 +1,17 @@
 package model
 
+import "gorm.io/gorm"
+
 // BaseExercise model
 type BaseExercise struct {
-	ID               uint8            `gorm:"column:id;type:mediumint unsigned;autoIncrement;primaryKey"`
+	gorm.Model
 	Name             string           `gorm:"column:name;type:varchar(25)"`
 	Description      string           `gorm:"column:description;type:varchar(60)"`
 	VideoURL         string           `gorm:"column:video_url;type:varchar(50)"`
-	CategoryID       uint8            `gorm:"column:category_id;type:mediumint unsigned"`
+	CategoryID       uint64           `gorm:"column:category_id;type:bigint(20) unsigned"`
 	Series           uint             `gorm:"column:series;type:tinyint unsigned"`
 	Repetitions      uint             `gorm:"column:repetitions;type:tinyint unsigned"`
-	ExerciseCategory ExerciseCategory `gorm:"foreignKey:CategoryID;references:ID"`
+	ExerciseCategory ExerciseCategory `gorm:"foreignKey:CategoryID;"`
 	Muscles          []*Muscle        `gorm:"many2many:exercises_muscles;"`
 }
 
--- backend/src/model/muscleModel.go
+++ backend/src/model/muscleModel.go
@@ -1,8 +1,10 @@
 package model
 
+import "gorm.io/gorm"
+
 // Muscle model
 type Muscle struct {
-	ID   uint8  `gorm:"column:id;type:mediumint unsigned;autoIncrement;primaryKey"`
+	gorm.Model
 	Name string `gorm:"column:name;type:varchar(20)"`
 }
 
