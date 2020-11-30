024152046d32a871db2675d89adeb2649a7a78a8
kdcr <kevincabrerarodriguez@gmail.com>
2020-11-30 11:41:08 +0000

    Removed mockup functions

--- backend/src/model/baseExerciseModel.go
+++ backend/src/model/baseExerciseModel.go
@@ -19,28 +19,3 @@ type BaseExercise struct {
 func (be *BaseExercise) TableName() string {
 	return "base_exercises"
 }
-
-// CreateBaseExercise Create base exercise
-func (be *BaseExercise) CreateBaseExercise() string {
-	return "Create a Base Exercise"
-}
-
-// GetBaseExercise Gets a base exercise
-func (be *BaseExercise) GetBaseExercise() string {
-	return "Get a BaseExercise"
-}
-
-// GetBaseExercises Gets all base exercise
-func (be *BaseExercise) GetBaseExercises() string {
-	return "Get all BaseExercises"
-}
-
-// UpdateBaseExercise Updates a base exercise
-func (be *BaseExercise) UpdateBaseExercise() string {
-	return "Update a BaseExxercises"
-}
-
-// DeleteBaseExercise Deletes a base exercise
-func (be *BaseExercise) DeleteBaseExercise() string {
-	return "Delete a BaseExercises"
-}
