2e5363eeb54efab041bf0b909d3280afc08fc63c
kdcr <kevincabrerarodriguez@gmail.com>
2020-12-02 12:28:00 +0000

    #36 development

--- backend/src/controller/routineCategoryController.go
+++ backend/src/controller/routineCategoryController.go
@@ -2,39 +2,79 @@ package controller
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/fullstacktf/fitness-backend/model"
+	"github.com/fullstacktf/fitness-backend/service"
 	"github.com/gin-gonic/gin"
 )
 
 var routineCategoryModel model.RoutineCategory
 
-// CreateRoutineCategory Creates a routine category
+// CreateRoutineCategory creates a category with the given data
 func CreateRoutineCategory(c *gin.Context) {
-	prueba := routineCategoryModel.CreateRoutineCategory()
-	fmt.Println(prueba)
+	newCategory := model.RoutineCategory{}
+	bodyErr := c.BindJSON(&newCategory)
+
+	err := service.CreateRoutineCategory(newCategory)
+
+	if bodyErr == nil {
+		if err != nil {
+			error := service.GetGormErrorCode(err.Error())
+
+			c.JSON(500, error)
+		} else {
+			c.String(200, "ok")
+		}
+	}
 }
 
-// GetRoutineCategory Gets a routine category
+// GetRoutineCategory Get category by id
 func GetRoutineCategory(c *gin.Context) {
-	prueba := routineCategoryModel.GetRoutineCategory()
-	fmt.Println(prueba)
+	id, _ := strconv.Atoi(c.Param("id"))
+	result := service.GetRoutineCategory(id)
+
+	if result.ID == 0 {
+		c.JSON(404, "Routine category not found")
+	} else {
+		c.JSON(200, result)
+	}
 }
 
-// GetRoutineCategories Gets all routine categories
+// GetRoutineCategories Get all non deleted categories and by using a filter
 func GetRoutineCategories(c *gin.Context) {
-	prueba := routineCategoryModel.GetRoutineCategories()
-	fmt.Println(prueba)
+	filter := model.RoutineCategory{}
+	reqErr := c.BindJSON(&filter)
+
+	if reqErr == nil {
+		result := service.GetRoutineCategories(filter)
+		c.JSON(200, result)
+	}
 }
 
-// UpdateRoutineCategory Updates a routine category
+// UpdateRoutineCategory Update specific category using id param in URL
 func UpdateRoutineCategory(c *gin.Context) {
-	prueba := routineCategoryModel.UpdateRoutineCategory()
-	fmt.Println(prueba)
+
+	updatedCategory := model.RoutineCategory{}
+	c.BindJSON(&updatedCategory)
+
+	err := service.UpdateRoutineCategory(updatedCategory)
+	if err != nil {
+		fmt.Println(err.Error())
+		error := service.GetGormErrorCode(err.Error())
+
+		c.JSON(500, error)
+	} else {
+		c.String(200, "ok")
+	}
 }
 
-// DeleteRoutineCategory Deletes a routine category
+// DeleteRoutineCategory Delete category by id, logical delete
 func DeleteRoutineCategory(c *gin.Context) {
-	prueba := routineCategoryModel.DeleteRoutineCategory()
-	fmt.Println(prueba)
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	service.DeleteRoutineCategory(id)
+
+	c.String(200, c.Param("id"))
+
 }
--- backend/src/model/routineCategoryModel.go
+++ backend/src/model/routineCategoryModel.go
@@ -12,28 +12,3 @@ type RoutineCategory struct {
 func (rc *RoutineCategory) TableName() string {
 	return "routine_category"
 }
-
-// CreateRoutineCategory Creates a routine category
-func (rc *RoutineCategory) CreateRoutineCategory() string {
-	return "Create a Routinecategory"
-}
-
-// GetRoutineCategory Gets a routine category
-func (rc *RoutineCategory) GetRoutineCategory() string {
-	return "Get a Routinecategory"
-}
-
-// GetRoutineCategories Gets all routine categories
-func (rc *RoutineCategory) GetRoutineCategories() string {
-	return "Get all Routinecategory"
-}
-
-// UpdateRoutineCategory Updates a routine category
-func (rc *RoutineCategory) UpdateRoutineCategory() string {
-	return "Update a Routinecategory"
-}
-
-// DeleteRoutineCategory Deletes a routine category
-func (rc *RoutineCategory) DeleteRoutineCategory() string {
-	return "Delete a Routinecategory"
-}
