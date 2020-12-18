81a68485c7ad57ee5b31aa7ca0681414be573838
Raul Rodriguez Torres <raulrodrigueztorres@protonmail.com>
2020-12-18 12:30:16 +0000

    Cookie domain added

--- backend/src/routes/routes.go
+++ backend/src/routes/routes.go
@@ -24,9 +24,10 @@ func SetupRouter() *gin.Engine {
 
 	r := gin.Default()
 	store := cookie.NewStore([]byte("secret"))
+	store.Options(sessions.Options{Domain: constants.DomainProduction})
 	r.Use(sessions.Sessions("mysession", store))
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://youlift.xyz", "http://youlift.xyz"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://youlift.xyz", "http://youlift2.xyz"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-type"},
 		ExposeHeaders:    []string{"Content-Length"},
